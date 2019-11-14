f2a1f3cc93e9e128876302b8deefb40db2ade228
nguyendt456 <nguyendt456@gmail.com>
2019-11-15 02:59:14 +0700

    Update 2.0

--- static/mysecret.js
+++ static/mysecret.js
@@ -1,18 +1,48 @@
-#this is /list js
-    $(document).ready(function(){
-            var socketio = io.connect('http://'+document.domain+':'+location.port+'/sensor');
-            var socket = io.connect('http://' + document.domain + ':' + location.port + '/user');
-            socket.emit('user','{{content.username}}');
-            socketio.on('temphum',function(msg){
-                console.log('#'+String(msg.MAC)+'temp');
-                $('#'+'temp'+String(msg.MAC)).html(msg.temp);
-                $('#'+'humidity'+String(msg.MAC)).html(msg.humidity);
-            });
+        $("#{{devices[i].uuid}}pass").keyup(function(event) {
+            if (event.keyCode === 13) {
+                $("#{{devices[i].uuid}}but").click();
+            }
         });
-        function drop(mac) {
+        function encode_utf8(s) {
+            return unescape(encodeURIComponent(s));
+        };
+        function drop(uuid) {
             let p = prompt('Hãy nhập mật khẩu để xác nhận');
             var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
                 xmlhttp.open("POST", "/list");
                 xmlhttp.setRequestHeader("Content-Type", "application/json");
-                xmlhttp.send(JSON.stringify({MAC:mac,p:p}));
+                xmlhttp.send(JSON.stringify({uuid:uuid,p:p}));
         };
+        function popup(uuid) {
+            if (document.getElementById(uuid+"prompt").style.display == "none") {
+                document.getElementById(uuid+"prompt").style.display = "block";
+            }
+            else document.getElementById(uuid+"prompt").style.display = "none";
+        };
+        function exe(uuid) {
+            document.getElementById(uuid+"prompt").style.display = "none";
+            var p = document.getElementById(uuid+"pass").value;
+            var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+            xmlhttp.open("POST", "/list");
+            xmlhttp.setRequestHeader("Content-Type", "application/json");
+            xmlhttp.send(JSON.stringify({'uuid':uuid,p:p}));
+        };
+        $(document).ready(function(){
+            var socketio = io.connect('http://'+document.domain+':'+location.port+'/sensor');
+            var socket = io.connect('http://' + document.domain + ':' + location.port + '/user');
+            socket.emit('user','{{content.username}}');
+            socketio.on('temphum',function(msg){
+                console.log('#'+String(msg.uuid)+'temp');
+                $('#'+'temp'+String(msg.uuid)).html(msg.temp);
+                $('#'+'humidity'+String(msg.uuid)).html(msg.humidity);
+            });
+            socketio.on('error',function(error){
+                if(error.success==' ') {
+                    alert(error.error);
+                }
+                if(error.error==' ') {
+                    alert(error.success);
+                    location.reload();
+                }
+            });
+        });
